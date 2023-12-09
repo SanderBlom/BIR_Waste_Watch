dd945c1f84eae9a0faed92934aa1cc012632f76e
Sander Blomvågnes <54621884+SanderBlom@users.noreply.github.com>
2023-12-09 12:42:43 +0100

    Added required domain parameter for the config_schema (#5)

--- custom_components/bir/__init__.py
+++ custom_components/bir/__init__.py
@@ -2,7 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 
-CONFIG_SCHEMA = cv.config_entry_only_config_schema()
+CONFIG_SCHEMA = cv.config_entry_only_config_schema("bir")
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
