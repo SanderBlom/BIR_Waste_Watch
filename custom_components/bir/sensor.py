bada059cfecbb75faf8ba2088ba04ec5dbf17938
SanderBlom <sander@blomvag.no>
2023-12-26 13:35:31 +0100

    Created a base class for both sensors

--- custom_components/bir/sensor.py
+++ custom_components/bir/sensor.py
@@ -1,10 +1,13 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 import aiohttp
 from homeassistant.components.sensor import SensorEntity
 import logging
-from .get_data import get_dates 
+from .get_data import get_dates
+
 _LOGGER = logging.getLogger(__name__)
+
 SCAN_INTERVAL = timedelta(hours=1)
+NA_STRING = "N/A"
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     url = config_entry.data.get("url")
@@ -20,8 +23,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     if data:
         sensors = []
         for waste_type, date in data.items():
-            collection_sensor = WasteCollectionSensorDates(session, url, waste_type, date, config_entry.entry_id)
-            days_until_sensor = WasteCollectionSensorDays(session, url, waste_type, date, config_entry.entry_id)
+            collection_sensor = WasteCollectionSensorDates(
+                session, url, waste_type, date, config_entry.entry_id
+            )
+            days_until_sensor = WasteCollectionSensorDays(
+                session, url, waste_type, date, config_entry.entry_id
+            )
             sensors.extend([collection_sensor, days_until_sensor])
 
             # Update the sensors on the first run
@@ -31,62 +38,61 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         if sensors:
             async_add_entities(sensors, True)
 
-class WasteCollectionSensorDates(SensorEntity):
-    def __init__(self, session, url, waste_type, state, entry_id):
+class WasteCollectionSensorBase(SensorEntity):
+    def __init__(self, session, url, waste_type, entry_id):
         self._session = session
         self._url = url
         self._waste_type = waste_type
-        self._state = state
         self._entry_id = entry_id
         self._last_updated = None
 
     @property
-    def entity_registry_enabled_default(self) -> bool:
-        return True
-
-    @property
     def unique_id(self):
-        return f"{self._entry_id}_{self._waste_type}"
+        raise NotImplementedError
 
-    @property
-    def name(self):
-        return f"{self._waste_type.replace('_', ' ').title()}"
-
-    @property
-    def state(self):
-        return self._state
-    
     @property
     def icon(self):
-        """Return the icon of the sensor."""
         return "mdi:trash-can"
 
     @property
     def extra_state_attributes(self):
-        """Return the state attributes."""
-        return {
-            'Last updated': self._last_updated
-        }
+        return {"Last updated": self._last_updated}
 
     async def async_update(self, *_):
         data = await get_dates(self._session, self._url)
         if data:
-            self._state = data.get(self._waste_type, "N/A")
-            self._last_updated = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self._last_updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-
-class WasteCollectionSensorDays(SensorEntity):
-    def __init__(self, session, url, waste_type, pickup_date, entry_id):
-        self._session = session
-        self._url = url
-        self._waste_type = waste_type
-        self._pickup_date = pickup_date
-        self._entry_id = entry_id
-        self._last_updated = None
+class WasteCollectionSensorDates(WasteCollectionSensorBase):
+    def __init__(self, session, url, waste_type, date, entry_id):
+        super().__init__(session, url, waste_type, entry_id)
+        self._date = date
+        self._state = NA_STRING
 
     @property
     def unique_id(self):
-        return f"{self._entry_id}_{self._waste_type}_days_until"
+        return f"{self._entry_id}_{self._waste_type}_date"
+
+    @property
+    def name(self):
+        return f"{self._waste_type.replace('_', ' ').title()} Collection Date"
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        data = await get_dates(self._session, self._url)
+        self._state = data.get(self._waste_type, NA_STRING) if data else NA_STRING
+
+class WasteCollectionSensorDays(WasteCollectionSensorBase):
+    def __init__(self, session, url, waste_type, date, entry_id):
+        super().__init__(session, url, waste_type, entry_id)
+        self._date = date
+
+    @property
+    def unique_id(self):
+        return f"{self._entry_id}_{self._waste_type}_days"
 
     @property
     def name(self):
@@ -96,25 +102,7 @@ class WasteCollectionSensorDays(SensorEntity):
     def state(self):
         return self._calculate_days_until_pickup()
 
-    @property
-    def icon(self):
-        return "mdi:trash-can"
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return {
-            'Last updated': self._last_updated
-        }
-
     def _calculate_days_until_pickup(self):
-        """Calculate the number of days until pickup."""
         today = datetime.now().date()
-        pickup_date_obj = datetime.strptime(self._pickup_date, '%Y-%m-%d').date()
+        pickup_date_obj = datetime.strptime(self._date, "%Y-%m-%d").date()
         return (pickup_date_obj - today).days
-
-    async def async_update(self):
-        data = await get_dates(self._session, self._url)
-        if data:
-            self._pickup_date = data.get(self._waste_type, "N/A")
-            self._last_updated = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
